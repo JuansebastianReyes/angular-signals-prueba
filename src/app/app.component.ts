ebc021b4ed81c672e22aae76a298f4716163d82b
Sebastian Reyes <juan.reyes@doublevpartners.com>
2024-05-16 19:53:20 -0500

    add example signals angular 17

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, computed, effect, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { toObservable } from '@angular/core/rxjs-interop';
+import { map } from 'rxjs';
+
+interface Task {
+  name: string,
+  isCompleted: boolean
+}
 
 @Component({
   selector: 'app-root',
@@ -9,6 +16,61 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'signals-example';
+
+  name = signal<string>("Juan");
+  description = signal<string>('signals-example').asReadonly();
+  tasks = signal<Task[]>([
+    {
+      name: "Curso signals",
+      isCompleted: false
+    }
+  ]);
+
+  task$ = toObservable(this.tasks);
+
+  taskLength = computed(() => this.tasks().length);
+
+  constructor() {
+    effect(
+      () => {
+        if(this.taskLength() > 3) alert('Tienes Muchas Tareas!!!')
+      });
+  }
+  ngOnInit(): void {
+    this.task$
+      .pipe(
+        map((res) => {
+          const newTask = res.map((task)=>({
+            ...task,
+            createDate: new Date(),
+          }));
+          return newTask
+        })
+      ).subscribe((res) => console.log(res));
+  }
+
+  togleName() {
+    this.name.set("Sebastian");
+  }
+
+  addTask() {
+    this.tasks.update((tasks)=>{
+      return [ ...tasks,
+        {
+          name: "Curso NestJs",
+          isCompleted: false
+        }
+      ]
+    })
+  }
+
+  markTask() {
+    this.tasks.update( tasks => {
+      this.tasks()[0].name = "Cambio Signal";
+      this.tasks()[0].isCompleted = false;
+      return tasks;
+    })
+  }
 }
